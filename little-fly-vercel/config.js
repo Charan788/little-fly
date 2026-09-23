f062dea4c940141733ea060db90b1a064201ee95
Charan788 <anumulacharan63@gmail.com>
2026-09-23 16:39:43 +0530

    Connect Little Fly to Google Analytics 4

--- little-fly-vercel/config.js
+++ little-fly-vercel/config.js
@@ -1,3 +1,3 @@
 // Paste your GA4 Measurement ID here, for example G-ABC1234567.
 // This is a public identifier, not an API key. Leave blank to disable analytics.
-window.LITTLE_FLY_CONFIG = { googleAnalyticsId: '' };
+window.LITTLE_FLY_CONFIG = { googleAnalyticsId: 'G-0GBXDR7KLF' };
